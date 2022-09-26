5227e9b545bcbcde15b05212feae6a488a285a89
Diego Higuera <dhiguerafernandez@gmail.com>
2022-09-26 11:06:31 -0300

    ADD project

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""Setup for table long input XBlock."""
+
+
+
+import os
+
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='tablelonginput-xblock',
+    version='0.1',
+    description='XBlock table with true/false questions',
+    license='AGPL v3',
+    packages=[
+        'tablelonginput',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'tablelonginput = tablelonginput:tablelonginputXBlock',
+        ]
+    },
+    package_data=package_data("tablelonginput", ["static", "public"]),
+)
